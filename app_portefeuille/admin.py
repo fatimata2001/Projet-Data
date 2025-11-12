db59c130799cc1b18b9640063fc50a1ea80b15e5
fatimata2001 <fatimata.mbayepro@gmail.com>
2025-11-12 09:21:57 +0100

    reprendre travaille fatim

--- app_portefeuille/admin.py
+++ app_portefeuille/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Actif, Portefeuille,  PortefeuilleActif
+
+# Afficher les lignes de PortefeuilleActif dans l'admin de Portefeuille
+class PortefeuilleActifEnligne(admin.TabularInline):
+    model = PortefeuilleActif
+    extra = 1  # Nombre de lignes supplémentaires vides à afficher
+    
--- app_portefeuille/views.py
+++ app_portefeuille/views.py
@@ -4,5 +4,7 @@ from .models import Portefeuille
 
 def index(request):
     # Crée ou récupère un portefeuille "Démo"
+    # pf : le portefeuille récupéré ou créé
+    # _ : reçois un booléen indiquant si le portefeuille a été créé ou non
     pf, _ = Portefeuille.objects.get_or_create(nom="Démo")
     return render(request, "app_portefeuille/index.html", {"portefeuille": pf})
